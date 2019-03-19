4973d9c0f023e00edac16136c40743dea6a38925
lifeinchords <steven.dale@protonmail.com>
2019-03-19 15:14:08 -0400

    trying a different way of scss compilation. Added concurrently to run both js and scss watch at the same time

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const bb = require('express-busboy')
 const https = require('https')
 const args = require('minimist')(process.argv.slice(2))
 const expressHandlebars  = require('express-handlebars')
-const sassMiddleware = require('node-sass-middleware')
 
 const app = express()
 app.use(cors())
@@ -22,21 +21,6 @@ app.engine(
 )
 app.set('view engine', 'handlebars')
 
-//  declare middeware before `express.static`. If not, it wont work 
-const srcPath = path.join(__dirname, 'scss')
-const destPath = path.join(__dirname, 'static', 'css')
-console.log('srcPath: ' + srcPath);
-console.log('destPath: ' + destPath);
-app.use(sassMiddleware({
-    src: srcPath,
-    dest: destPath,
-    debug: true,
-    outputStyle: 'expanded',
-
-    // <link rel="stylesheets" href="prefix/style.css"/>
-    prefix:  '/css'  
-}))
-
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.use(bodyParser.json({limit: '337mb'}))
@@ -44,7 +28,7 @@ app.use(bodyParser.urlencoded({
   extended: false,
   limit: '50mb'
 }))
-// app.use(express.static('static'))
+app.use(express.static('static'))
 
 app.use(router)
 
