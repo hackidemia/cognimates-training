046161ff7a361c374124b9abb75a2f7e4850f3ed
lifeinchords <steven.dale@protonmail.com>
2019-03-19 14:19:48 -0400

    Added nodemon for dev file watching. Added SASS compilation and updated CSS files to SCSS. Removed unused references + files.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -7,28 +8,43 @@ const config = require('./config')
 const bb = require('express-busboy')
 const https = require('https')
 const args = require('minimist')(process.argv.slice(2))
-const expressHandlebars  = require('express-handlebars');
+const expressHandlebars  = require('express-handlebars')
+const sassMiddleware = require('node-sass-middleware')
 
 const app = express()
 app.use(cors())
 
-// good example of using handlebars in more detail
-// repo: https://github.com/ericf/express-handlebars/tree/master/examples/advanced
 app.engine(
   'handlebars',
   expressHandlebars({
     defaultLayout: 'main'
   })
-);
-app.set('view engine', 'handlebars');
+)
+app.set('view engine', 'handlebars')
 
-app.use(express.static('static'));
+//  declare middeware before `express.static`. If not, it wont work 
+const srcPath = path.join(__dirname, 'scss')
+const destPath = path.join(__dirname, 'static', 'css')
+console.log('srcPath: ' + srcPath);
+console.log('destPath: ' + destPath);
+app.use(sassMiddleware({
+    src: srcPath,
+    dest: destPath,
+    debug: true,
+    outputStyle: 'expanded',
+
+    // <link rel="stylesheets" href="prefix/style.css"/>
+    prefix:  '/css'  
+}))
+
+app.use(express.static(path.join(__dirname, 'static')))
+
 app.use(bodyParser.json({limit: '337mb'}))
 app.use(bodyParser.urlencoded({
   extended: false,
   limit: '50mb'
 }))
-app.use(express.static('static'))
+// app.use(express.static('static'))
 
 app.use(router)
 
@@ -38,17 +54,18 @@ bb.extend(app, {
 
 if(args.http == true) {
   app.listen(config.SERVER_PORT, () => {
-    console.log(`Server running at http://localhost:${config.SERVER_PORT}`);
+    console.log(`Server running at http://localhost:${config.SERVER_PORT}`)
   })
 } else {
-  const config = require('./config');
+  const config = require('./config')
   var options = {
     ca: fs.readFileSync(config.sslCA),
     key: fs.readFileSync(config.sslKeyPath),
     cert: fs.readFileSync(config.sslCertPath)
   }
-  var server = https.createServer(options, app);
+  var server = https.createServer(options, app)
   server.listen(config.SERVER_PORT, () => {
-    console.log(`Server running http://localhost:${config.SERVER_PORT}`);
-  });
+    console.log(`Server running http://localhost:${config.SERVER_PORT}`)
+  })
 }
+
