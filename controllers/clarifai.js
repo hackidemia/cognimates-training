ac1839cf7d53ea8d0dc1d064da01ee997ee73ba3
satvu <sarahvu@mit.edu>
2019-04-20 17:44:51 -0400

    get key from header not body

--- controllers/clarifai.js
+++ controllers/clarifai.js
@@ -234,11 +234,10 @@ function classifyImage(req, res) {
 }
 
 function classifyURLImage(req, res){
-  const apiKey = req.body.apikey;
+  const apiKey = req.headers.apikey;
   var image_link = req.body.image_url;
   const model_id = req.body.classifier_id;
   const app = init(apiKey);
-  console.log('here');
   app.models.predict(model_id, { url: image_link }).then(
     (response) =>{
       if(parseInt(response.status.code) == 10000) {
